8117bc9815361f7a83d1698284731441d723f111
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-01 14:13:36 -0500

    Añadir archivos

---
+++ src/main/java/literalura/Challenge_Three/ChallengeThreeApplication.java
@@ -0,0 +1,13 @@
+package literalura.Challenge_Three;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeThreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeThreeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/literalura/Challenge_Three/ChallengeThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package literalura.Challenge_Three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
