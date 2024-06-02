27e5cd1b236b4bf45b30ceee273f89777b18fd95
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-01 19:59:58 -0500

    Actualizar ChallengeThreeApplication.java

--- src/main/java/literalura/Challenge_Three/ChallengeThreeApplication.java
+++ src/main/java/literalura/Challenge_Three/ChallengeThreeApplication.java
@@ -1,13 +1,20 @@
 package literalura.Challenge_Three;
 
+import literalura.Challenge_Three.principal.Principal;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ChallengeThreeApplication {
+public class ChallengeThreeApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeThreeApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.mostrarMenu();
+	}
 }
