175601c390fb94c1e2ca164c76ab35f2bcf10aac
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-03 20:00:23 -0500

    Actualizar entidades

--- src/main/java/literalura/Challenge_Three/modelo/Autor.java
+++ src/main/java/literalura/Challenge_Three/modelo/Autor.java
@@ -1,20 +1,37 @@
 package literalura.Challenge_Three.modelo;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 
+@Entity
+@Table(name = "autores")
 public class Autor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nombre;
     private Integer nacimiento;
     private Integer fallecimiento;
 
+    public Autor(){}
+
     public Autor(DatosAutor datosAutor) {
         this.nombre=datosAutor.nombre();
         this.nacimiento=datosAutor.nacimiento();
-        this.fallecimiento=evaluarFecha(datosAutor.fallecimiento());
+        this.fallecimiento=Objects.nonNull(datosAutor.fallecimiento()) ? datosAutor.fallecimiento() : 0;
     }
 
-    private Integer evaluarFecha(Integer fecha){
-        return Objects.nonNull(fecha) ? fecha : 0;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setNacimiento(Integer nacimiento) {
+        this.nacimiento = nacimiento;
+    }
+
+    public void setFallecimiento(Integer fallecimiento) {
+        this.fallecimiento = fallecimiento;
     }
 
     public String getNombre() {
--- src/main/java/literalura/Challenge_Three/modelo/Libro.java
+++ src/main/java/literalura/Challenge_Three/modelo/Libro.java
@@ -1,26 +1,52 @@
 package literalura.Challenge_Three.modelo;
 
+import jakarta.persistence.*;
+
 import java.util.List;
 import java.util.Optional;
 
+@Entity
+@Table(name = "libros")
 public class Libro {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String titulo;
+    @Enumerated(EnumType.STRING)
     private Idioma idioma;
     private Integer numeroDeDescargas;
+    @ManyToOne(fetch = FetchType.EAGER)
     private Autor autor;
+
+    public Libro(){}
 
     public Libro(DatosLibro datosLibro) {
         this.titulo=datosLibro.titulo();
         this.idioma=Idioma.fromString(datosLibro.idioma().get(0));
         this.numeroDeDescargas=datosLibro.numeroDeDescargas();
-        this.autor=evaluarAutor(datosLibro.autor());
     }
 
-    private Autor evaluarAutor(List<DatosAutor> datosAutor){
+    public static Autor evaluarAutor(List<DatosAutor> datosAutor){
         Optional<DatosAutor> autor = datosAutor.stream()
                 .filter(a->a.nacimiento()!=null)
                 .findFirst();
         return autor.map(Autor::new).orElse(null);
+    }
+
+    public void setAutor(Autor autor) {
+        this.autor = autor;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setIdioma(Idioma idioma) {
+        this.idioma = idioma;
+    }
+
+    public void setNumeroDeDescargas(Integer numeroDeDescargas) {
+        this.numeroDeDescargas = numeroDeDescargas;
     }
 
     public String getTitulo() {
