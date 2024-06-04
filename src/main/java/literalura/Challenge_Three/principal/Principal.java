508d152b216d092b1a383712549e036d4deef877
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-03 20:02:09 -0500

    Actualizar Principal.java

--- src/main/java/literalura/Challenge_Three/principal/Principal.java
+++ src/main/java/literalura/Challenge_Three/principal/Principal.java
@@ -1,12 +1,12 @@
 package literalura.Challenge_Three.principal;
 
-import literalura.Challenge_Three.modelo.Autor;
-import literalura.Challenge_Three.modelo.Datos;
-import literalura.Challenge_Three.modelo.DatosLibro;
-import literalura.Challenge_Three.modelo.Libro;
+import literalura.Challenge_Three.modelo.*;
+import literalura.Challenge_Three.repositorio.AutorRepositorio;
+import literalura.Challenge_Three.repositorio.LibroRepositorio;
 import literalura.Challenge_Three.servicio.ConsumirApi;
 import literalura.Challenge_Three.servicio.ConvertirDatos;
 
+import java.time.LocalDate;
 import java.util.*;
 
 public class Principal {
@@ -16,22 +16,35 @@ public class Principal {
     private ConsumirApi consumir = new ConsumirApi();
     private ConvertirDatos convertir = new ConvertirDatos();
     private final String URL_BASE = "https://gutendex.com/books/";
+    private AutorRepositorio autorRepositorio;
+    private LibroRepositorio libroRepositorio;
+
+    public Principal(
+            AutorRepositorio autorRepositorio,
+            LibroRepositorio libroRepositorio
+    ) {
+        this.autorRepositorio = autorRepositorio;
+        this.libroRepositorio = libroRepositorio;
+    }
 
     public void mostrarMenu(){
         int opcion = -1;
         while (opcion != 0){
             String menu = """
+                    ********************* MENÚ *********************
                     1. Buscar libro por su título
                     2. Mostrar libros buscados
                     3. Mostrar autores
                     4. Buscar autores vivos en un determinado año
-                    
+                    5. Cantidad de libros en un determinado idioma
                     0. Salir
+                    ************************************************
                     """;
             System.out.print(menu);
             System.out.print("Ingresar opción: ");
             opcion = lectura.nextInt();
             lectura.nextLine();
+            System.out.println();
             switch (opcion){
                 case 0:
                     System.out.println(">> Salió exitosamente.");
@@ -48,7 +61,11 @@ public class Principal {
                 case 4:
                     buscarAutoresVivos();
                     break;
+                case 5:
+                    cantidadLibros();
+                    break;
             }
+            System.out.println();
         }
 
     }
@@ -61,38 +78,82 @@ public class Principal {
         Optional<DatosLibro> datosLibro = datos.libro().stream()
                 .findFirst();
         if(datosLibro.isPresent()){
-            System.out.println(">> Libro encontrado:");
+            System.out.println("\n>> Libro encontrado:");
+            Autor autor = Libro.evaluarAutor(datosLibro.get().autor());
             Libro libro = new Libro(datosLibro.get());
-            listaDeLibros.add(libro);
-            listaDeAutores.add(libro.getAutor());
+            Optional<Libro> buscarLibro = libroRepositorio.findAll().stream()
+                    .filter(l->l.getTitulo().contains(libro.getTitulo()))
+                    .findFirst();
+            if (buscarLibro.isEmpty()){
+                Optional<Autor> buscarAutor = autorRepositorio.findByNombre(autor.getNombre());
+                Autor autorBD;
+                if (buscarAutor.isPresent()){
+                    autorBD = buscarAutor.get();
+                }else{
+                    autorBD = autorRepositorio.save(autor);
+                }
+                libro.setAutor(autorBD);
+                libroRepositorio.save(libro);
+            }
+            libro.setAutor(autor);
             System.out.println(libro);
         }else{
-            System.out.println(">> Libro no encontrado.");
+            System.out.println("\n>> Libro no encontrado.");
         }
     }
 
     private void mostrarLibros(){
-        if (!listaDeLibros.isEmpty()){
-            listaDeLibros.forEach(System.out::println);
-        }else {
-            System.out.println(">> Aún no se han buscado libros.");
-        }
+        listaDeLibros = libroRepositorio.findAll();
+        System.out.println(">> Lista de libros registrados:");
+        mostrar(listaDeLibros);
     }
 
     private void mostrarAutores(){
-        if (!listaDeAutores.isEmpty()){
-            listaDeAutores.forEach(System.out::println);
-        }else {
-            System.out.println(">> Aún no se han buscado libros.");
-        }
+        listaDeAutores = autorRepositorio.findAll();
+        System.out.println(">> Lista de autores registrados:");
+        mostrar(listaDeAutores);
     }
 
     private void buscarAutoresVivos(){
-        System.out.print("Ingresar año: ");
-        Integer fecha = lectura.nextInt();
+        int fechaHoy = LocalDate.now().getYear();
+        Integer fecha;
+        do{
+            System.out.print("Ingresar año: ");
+            fecha = lectura.nextInt();
+        }while(fecha>fechaHoy||fecha<-600);
         lectura.nextLine();
-        listaDeAutores.stream()
-                .filter(a->a.getFallecimiento()>=fecha||a.getFallecimiento()==0)
-                .forEach(System.out::println);
+        Optional<List<Autor>> buscarAutores = autorRepositorio.findByFallecimientoGreaterThanAndNacimientoLessThanEqual(fecha,fecha);
+        if(buscarAutores.isPresent()){
+            List<Autor> autoresBD = buscarAutores.get();
+            autoresBD.forEach(System.out::println);
+            if(autoresBD.isEmpty()){
+                System.out.println("\n>> No hay autores vivos en el año "+fecha+".");
+            }
+        }
+    }
+
+    private void cantidadLibros() {
+        System.out.print("Ingresar idioma: ");
+        Idioma buscarIdioma = Idioma.fromSpanish(lectura.nextLine());
+        Optional<List<Libro>> buscarLibros = libroRepositorio.findByIdioma(buscarIdioma);
+        if (buscarLibros.isPresent()){
+            List<Libro> librosBD = buscarLibros.get();
+            int cantidad = librosBD.size();
+            if(cantidad!=0){
+                String texto = (cantidad==1)?" libro registrado":" libros registrados";
+                System.out.println("\n>> Hay "+cantidad+texto+":");
+                librosBD.forEach(System.out::println);
+            }else{
+                System.out.println("\n>> Hay 0 libros registrados.");
+            }
+        }
+    }
+
+    private <T> void mostrar(List<T> lista){
+        if (!lista.isEmpty()){
+            lista.forEach(System.out::println);
+        }else {
+            System.out.println("\n>> Aún no se han buscado libros.");
+        }
     }
 }
