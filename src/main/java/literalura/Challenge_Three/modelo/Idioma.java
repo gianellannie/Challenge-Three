73a4a9c066b9edf83a1a75de0bd71f8f869c43eb
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-03 20:01:39 -0500

    Actualizar Idioma.java

--- src/main/java/literalura/Challenge_Three/modelo/Idioma.java
+++ src/main/java/literalura/Challenge_Three/modelo/Idioma.java
@@ -1,24 +1,35 @@
 package literalura.Challenge_Three.modelo;
 
 public enum Idioma {
-    INGLES("en"),
-    FRANCES("fr"),
-    FINLANDES("fi"),
-    ESPANOL("es"),
-    PORTUGUES("pt"),
-    ITALIANO("it"),
-    ALEMAN("de"),
-    RUSO("ru");
+    INGLES("en","inglés"),
+    FRANCES("fr","francés"),
+    FINLANDES("fi","finlandés"),
+    ESPANOL("es","español"),
+    PORTUGUES("pt","portugués"),
+    ITALIANO("it","italiano"),
+    ALEMAN("de","alemán"),
+    RUSO("ru","ruso");
 
-    private String idioma;
+    private String idiomaJSON;
+    private String idiomaBuscar;
 
-    Idioma(String idioma){
-        this.idioma=idioma;
+    Idioma(String idiomaJSON,String idiomaBuscar){
+        this.idiomaJSON=idiomaJSON;
+        this.idiomaBuscar=idiomaBuscar;
     }
 
     public static Idioma fromString(String texto) {
         for (Idioma i : Idioma.values()) {
-            if (i.idioma.equalsIgnoreCase(texto)) {
+            if (i.idiomaJSON.equalsIgnoreCase(texto)) {
+                return i;
+            }
+        }
+        throw new IllegalArgumentException("Ningún idioma encontrado: " + texto);
+    }
+
+    public static Idioma fromSpanish(String texto) {
+        for (Idioma i : Idioma.values()) {
+            if (i.idiomaBuscar.equalsIgnoreCase(texto)) {
                 return i;
             }
         }
