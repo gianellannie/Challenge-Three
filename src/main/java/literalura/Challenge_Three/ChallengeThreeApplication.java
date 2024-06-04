10aa8f118c6a166e1f10de8e4aa3d087ce5ed3bb
Gianella Annie <gianella.annie.060@gmail.com>
2024-06-03 20:02:44 -0500

    Actualizar archivos

--- src/main/java/literalura/Challenge_Three/ChallengeThreeApplication.java
+++ src/main/java/literalura/Challenge_Three/ChallengeThreeApplication.java
@@ -1,6 +1,9 @@
 package literalura.Challenge_Three;
 
 import literalura.Challenge_Three.principal.Principal;
+import literalura.Challenge_Three.repositorio.AutorRepositorio;
+import literalura.Challenge_Three.repositorio.LibroRepositorio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,13 +11,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ChallengeThreeApplication implements CommandLineRunner {
 
+	@Autowired
+	private AutorRepositorio autorRepositorio;
+
+	@Autowired
+	private LibroRepositorio libroRepositorio;
+
 	public static void main(String[] args) {
 		SpringApplication.run(ChallengeThreeApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Principal principal = new Principal();
+		Principal principal = new Principal(autorRepositorio,libroRepositorio);
 		principal.mostrarMenu();
 	}
 }
